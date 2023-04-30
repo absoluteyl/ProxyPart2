b28b3757fed847c102dc0480ef6c82b9c3076c66
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-04-30 20:38:20 +0800

    add instructions
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/BasicProxy.sol
+++ src/BasicProxy.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.17;
 import { Proxy } from "./Proxy/Proxy.sol";
 import { Slots } from "./SlotManipulate.sol";
 
+// Hands-on: https://docs.google.com/presentation/d/1lFveL2KWquktUjI7asbuedwy19tPBw66sxG8XxUOlJo/edit#slide=id.g21ea8db5214_7_48
+// 請根據 ERC1967 標準實作：
+// 請將 BasicProxy 改寫，將 Logic contract 存在bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1) 這個 Slot
+// 改寫 upgradeTo 和 upgradeToAndCall 的邏輯
+// 同樣使用 Clock 和 ClockV2 來測試 Upgrade 是否成功
 contract BasicProxy is Proxy, Slots {
 
   constructor(address _implementation) {
