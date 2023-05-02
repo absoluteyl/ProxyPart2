701751f64ca837e788acb0752f8664ec280acd6f
absoluteyl <17698254+absoluteyl@users.noreply.github.com>
2023-05-02 11:39:54 +0800

    update proxiable contract to to check if implementation contract supports UUPS
    Signed-off-by: absoluteyl <17698254+absoluteyl@users.noreply.github.com>

--- src/Proxy/Proxiable.sol
+++ src/Proxy/Proxiable.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.17;
 contract Proxiable {
 
   function proxiableUUID() public pure returns (bytes32) {
-    return 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7;
+    bytes32 _slot = keccak256("PROXIABLE");
+    // 0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7
+    return _slot;
   }
 
   function updateCodeAddress(address newAddress) internal {
-    // TODO: check if target address has proxiableUUID
+    // check if target address has proxiableUUID
+    (bool success,) = newAddress.delegatecall(abi.encodeWithSignature("proxiableUUID()"));
+    require(success, "Not Proxiable");
     // update code address
+    bytes32 proxiableSlot= proxiableUUID();
+    assembly {
+      sstore(proxiableSlot, newAddress)
+    }
   }
 }
